05073d1bbff5da5e4dfd1faf9456ac6d90d2b633
Roque Rueda <rrueda@itexico.net>
2018-04-12 11:05:28 -0500

    4 Add Non Terminal Expression

--- InterpreterPattern/Interpret/NonTerminalExpression.swift
+++ InterpreterPattern/Interpret/NonTerminalExpression.swift
@@ -8,6 +8,17 @@
 
 import UIKit
 
-class NonTerminalExpression: NSObject {
-
+class NonTerminalExpression: NSObject, AbstractExpression {
+    
+    private var prev : AbstractExpression
+    private var next : AbstractExpression
+    
+    init(withExpressions prev: AbstractExpression, next: AbstractExpression) {
+        self.prev = prev
+        self.next = next
+    }
+    
+    func interprete(ctx: InterpreterContext) -> Double {
+        return 0;
+    }
 }
